edba6385f4ee92bf03bf3064f1ff16b98815583b
Jorge Rios <jorge.rios@REM-MC-210813A.local>
2021-09-15 13:28:13 -0500

    trabajando en diseño /home

--- website/infrastructure/10_rds.tf
+++ website/infrastructure/10_rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "production" {
   password                = var.rds_password
   port                    = "5432"
   engine                  = "postgres"
-  engine_version          = "12.5"
+  engine_version          = "12.7"
   instance_class          = var.rds_instance_class
   allocated_storage       = "20"
   storage_encrypted       = false
